9236ee5c0ebf20088b25d0dde7e1226befc22b35
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-08-25 14:25:30 -0700

    Basic route setup complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-// import Home from './Components/Home/Home';
 import routes from './routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
-        {routes()}
+        {routes}
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import About from './Components/About/About';
 
-export default function routes(){
-    return <Switch>
+export default (
+        <Switch>
             <Route exact path="/" component={Home}/>
+            <Route path='/about' component={About}/>
         </Switch>
-}
+)
