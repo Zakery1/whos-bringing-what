d31925c9deff5e94f712d8a3345d79e7c10fd478
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-08-26 16:37:26 -0700

    Implement DB with requesteditems, invitations, events

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS users CASCADE; 
+DROP TABLE IF EXISTS events CASCADE;
+DROP TABLE IF EXISTS requesteditems CASCADE;
+DROP TABLE IF EXISTS invitations CASCADE;
 
 CREATE TABLE users (
 id SERIAL PRIMARY KEY
@@ -8,9 +11,58 @@ id SERIAL PRIMARY KEY
 , profile_pic TEXT
 );
 
+CREATE TABLE events (
+id SERIAL PRIMARY KEY
+, event_id TEXT
+, event_name TEXT
+, cover_photo TEXT
+, description TEXT
+, place TEXT
+, start_time INTEGER
+, user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE requesteditems (
+id SERIAL PRIMARY KEY
+, name TEXT
+, event_id INTEGER REFERENCES events(id)
+, user_id INTEGER REFERENCES users(id)
+, spokenfor BOOLEAN
+);
+
+CREATE TABLE invitations (
+id SERIAL PRIMARY KEY
+, event_id INTEGER REFERENCES events(id)
+, user_id INTEGER REFERENCES users(id)
+);
+
 -- Dummy Data
 
 INSERT INTO users 
 (auth0_id,email,username,profile_pic)
 VALUES
-('123abc','gmail@andrew.com','andrew','http://www.readersdigest.ca/wp-content/uploads/2013/03/6-facts-to-know-before-owning-a-puppy.jpg');
+('123abc','gmail@andrew.com','andrew','http://www.readersdigest.ca/wp-content/uploads/2013/03/6-facts-to-know-before-owning-a-puppy.jpg')
+, ('123ade', 'gmail@zak.com', 'zak', 'https://snworksceo.imgix.net/dtc/10ec0a64-8f9d-46d9-acee-5ef9094d229d.sized-1000x1000.jpg');
+
+INSERT INTO events 
+(event_id, event_name, cover_photo, description, place, start_time, user_id)
+VALUES 
+('5', 'BBQ', 'https://s3-media4.fl.yelpcdn.com/bphoto/ZMi0ykSsPismX_M-0cT6lw/o.jpg', 'Grilled Jellyfish for Abraham', 'at my place', 5, 1);
+
+INSERT INTO requesteditems 
+(name, event_id, user_id, spokenfor)
+VALUES
+('jellyfish', 1, 1, false)
+, ('pizza', 1, 2, true);
+
+INSERT INTO invitations 
+(event_id, user_id)
+VALUE
+(1,1)
+, (1,2);
+
+-- Select ALL
+SELECT * FROM users;
+SELECT * FROM events;
+SELECT * FROM requesteditems;
+SELECT * FROM invitations;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/main.css';
 import { BrowserRouter } from 'react-router-dom';
 // import registerServiceWorker from './registerServiceWorker';
 
