77a872d9170e6dc195abe9f43240f09095887c0c
Zachary Graham <zakgraham@gmail.com>
2018-08-28 13:53:12 -0700

    fix mobile vabar to bottom

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,12 +6,12 @@ function Footer() {
       <div className="Footer_parent">
       <div className="Footer_parent small">
         <h1 className="Footer_appName">Who's Bringing What?</h1>
-        <p className="Footer_aboutParagraph">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in.</p>
+        <p className="Footer_aboutParagraphMobile">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in.</p>
       </div>
 
       <div className="Footer_parent big">
         <h1 className="Footer_appName">Who's Bringing What?</h1>
-        <p className="Footer_aboutParagraph">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in.</p>
+        <p className="Footer_aboutParagraphDesktop">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in.</p>
       </div>
       </div>
     );
