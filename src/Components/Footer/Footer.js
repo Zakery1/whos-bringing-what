52db1b9c667b42b2921b8e592db33cb4ef63544d
Zachary Graham <zakgraham@gmail.com>
2018-08-28 17:05:02 -0700

    reformated desktop home

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,15 +4,15 @@ import React from 'react';
 function Footer() {
     return (
       <div className="Footer_parent">
-      <div className="Footer_parent small">
-        <h1 className="Footer_appName">Who's Bringing What?</h1>
-        <p className="Footer_aboutParagraphMobile">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in.</p>
-      </div>
+          <div className="Footer_parent small">
+            <h1 className="Footer_appName">Who's Bringing What?</h1>
+            <p className="Footer_aboutParagraphMobile">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in.</p>
+          </div>
 
-      <div className="Footer_parent big">
-        <h1 className="Footer_appName">Who's Bringing What?</h1>
-        <p className="Footer_aboutParagraphDesktop">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in.</p>
-      </div>
+          <div className="Footer_parent big">
+            <h1 className="Footer_appName">Who's Bringing What?</h1>
+            <p className="Footer_aboutParagraphDesktop">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in. </p>
+          </div>
       </div>
     );
   }
