9575e61d8a40d5a9730c8e1954da693e8c17d04f
Danielle Cucinotta <Danielle.cucinotta@gmail.com>
2018-08-29 17:24:53 -0700

    feed css

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,12 +6,12 @@ function Footer() {
       <div className="Footer_parent">
           <div className="Footer_parent small">
             <h1 className="Footer_appName">Who's Bringing What?</h1>
-            <p className="Footer_aboutParagraphMobile">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in.</p>
+            <p className="Footer_aboutParagraphMobile">There’s nothing better than getting together with your friends, but the process of planning events can be a surefire way to get a headache.  That’s where “Who's Bringing What?” comes in.</p>
           </div>
 
           <div className="Footer_parent big">
             <h1 className="Footer_appName">Who's Bringing What?</h1>
-            <p className="Footer_aboutParagraphDesktop">There’s nothing better than everyone getting together and celebrating, but the process of planning events is a surefire way to get a headache.  That’s where “Who Bring’s What?” comes in. </p>
+            <p className="Footer_aboutParagraphDesktop">There’s nothing better than getting together with your friends, but the process of planning events can be a surefire way to get a headache.  That’s where “Who's Bringing What?” comes in.</p>
           </div>
       </div>
     );
