dc27e138edfaf0e2efe3da77d2400c8849beeb8f
Danielle Cucinotta <Danielle.cucinotta@gmail.com>
2018-08-31 16:49:22 -0700

    don't show past events

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -7,38 +7,48 @@ Sugar.Date.extend()
 
 
 class Feed extends Component {
-  state = {}
+  constructor(){
+    super();
+    this.state = {
+    }
+  }
   // DaysUntil=new Date().daysUntil({event.start_time})
   render() {
-    console.log(Sugar + "")
+    // console.log(Sugar + "")
     return (
       <div className="main">
       <div>
       <div className="Feed_parent small">
         <div className="Feed_title">Who's Bringing What? </div>
         <Fetcher url='/api/createdEvents' render={(data) => {
-          return ( 
-            <div >
-              <div className="Feed_blank_small"></div>
-              <div className="Feed_event">Events I'm hosting:</div>
-              {data.map((event,i) => <Link to={`/creatorSpecificEvent/${event.id}`} key={i}><div>
-              <div className="Feed_blank_small"></div>
-              <div className="Feed_group">
-              <div className="Feed_event_name">{event.event_name}</div>
-              <div><img className='Feed_eventPhoto' src={event.cover_photo} alt="Displaying event portrait"/></div>
-              <div className="Feed_event_description">{<ReadMoreReact text={event.description ? event.description : ''} max={100} />}</div>
-              <div className="Feed_event_date">Event starts in {new Date().daysUntil(event.start_time)} days!!!</div>
-              </div>
-              </div></Link>)}
-            </div>
-          )
-        }}/>
+        const result = data.filter(thing => new Date(thing.start_time).isPast() === false)
+        return ( 
+          <div>
+                <div className="Feed_blank_small"></div>
+                <div className="Feed_event">Events I'm hosting:</div>
+            {result.map((event,i) => {
+                  return(
+                    <Link to={`/creatorSpecificEvent/${event.id}`} key={i}>
+                      <div className="Feed_blank_small"></div>
+                      <div className="Feed_group">
+                      <div className="Feed_event_name">{event.event_name}</div>
+                      <div><img className='Feed_eventPhoto' src={event.cover_photo} alt="Displaying event portrait"/></div>
+                      <div className="Feed_event_description">{<ReadMoreReact text={event.description ? event.description : ''} max={100} />}</div>
+                      <div className="Feed_event_date">Event starts in {new Date().daysUntil(event.start_time)} days!!!</div>
+                    </div></Link>)}
+            )}
+          </div>
+                )}}/>
+
         <Fetcher url='/api/invitedEvents' render={(data) => {
+          const result = data.filter(thing => new Date(thing.start_time).isPast() === false)
           return ( 
             <div>
               <div className="Feed_blank_small"></div>
               <div className="Feed_event">Events I'm invited to:</div>
-              {data.map((event,i) => <Link to={`/specificEvent/${event.id}`} key={i}><div>
+              {result.map((event,i) => {
+                return(
+              <Link to={`/specificEvent/${event.id}`} key={i}><div>
               <div className="Feed_blank_small"></div>
               <div className="Feed_group">
               <div className="Feed_event_name">{event.event_name}</div>
@@ -47,9 +57,9 @@ class Feed extends Component {
               <div className="Feed_event_date">Event starts in {new Date().daysUntil(event.start_time)} days!!</div>
               </div>
               </div></Link>)}
+              )}
               <div className="Feed_footer"></div>
             </div>
-        
           )
         }}/>
         </div>
@@ -60,11 +70,12 @@ class Feed extends Component {
       <div className="Feed_parent big">
         <div className="Feed_title">Who's Bringing What?</div>
         <Fetcher url='/api/createdEvents' render={(data) => {
+          const result = data.filter(thing => new Date(thing.start_time).isPast() === false)
           return ( 
             <div>
               <div className="Feed_blank"></div>
               <div className="Feed_event"><p>Events I'm hosting:</p></div>
-              {data.map((event,i) => <Link to={`/creatorSpecificEvent/${event.id}`} key={i}><div>
+              {result.map((event,i) => <Link to={`/creatorSpecificEvent/${event.id}`} key={i}><div>
               <div className="Feed_blank"></div>
               <div className="Feed_group">
               <div className="Feed_event_name">{event.event_name}</div>
@@ -77,11 +88,12 @@ class Feed extends Component {
           )
         }}/>
         <Fetcher url='/api/invitedEvents' render={(data) => {
+          const result = data.filter(thing => new Date(thing.start_time).isPast() === false)
           return ( 
             <div>
               <div className="Feed_blank"></div>
               <div className="Feed_event"><p>Events I'm invited to:</p></div>
-              {data.map((event,i) => <Link to={`/specificEvent/${event.id}`} key={i}><div>
+              {result.map((event,i) => <Link to={`/specificEvent/${event.id}`} key={i}><div>
               <div className="Feed_blank"></div>
               <div className="Feed_group">
               <div className="Feed_event_name">{event.event_name}</div>
@@ -91,13 +103,11 @@ class Feed extends Component {
               </div>
               </div></Link>)}
             </div>
-        
           )
         }}/>
         </div>
         </div>
         </div>
-
     );
   }
 }
