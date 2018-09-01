5aa2dadba6427b5f55dec2a135b206ca0e3fd89b
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-09-01 16:16:23 -0700

    Fix facebook accessToken issue

--- server/controller/user_controller.js
+++ server/controller/user_controller.js
@@ -126,8 +126,9 @@ module.exports = {
                     })
                 })
             })
+            return facebookAccessTokenResponse;
         }
-        function checkDatabaseEventsForUnattend() {
+        function checkDatabaseEventsForUnattend(facebookAccessTokenResponse) {
             let newAuth0Id = req.session.user.sub.split('|')[1]
                 dbInstance.read_events_link_to_user([newAuth0Id.toString()]).then(databaseEvents => {
                     axios.get(`https://graph.facebook.com/me?fields=events{id,name,cover,description,place,rsvp_status,start_time,admins}&access_token=${facebookAccessTokenResponse.data.identities[0].access_token}`)
