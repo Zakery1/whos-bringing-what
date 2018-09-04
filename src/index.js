b078828cba8ce31a9461fef42fe166395daa4bcc
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-09-04 10:49:27 -0700

    Add hot-reload

--- src/index.js
+++ src/index.js
@@ -5,13 +5,27 @@ import './styles/main.css'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-<Provider store={store}>
-<BrowserRouter>    
-<App />
-</BrowserRouter>
-</Provider>
-, document.getElementById('root'));
-// registerServiceWorker();
+import { AppContainer } from 'react-hot-loader';
+// import registerServiceWorker from './registerServiceWorker';
+const render = () => {
+    ReactDOM.render(
+    <AppContainer>
+        <Provider store={store}>
+            <BrowserRouter>    
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </AppContainer>
+    , document.getElementById('root'));
+    // registerServiceWorker();
+}
+
+
+render(); 
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+      render();
+    });
+  }
