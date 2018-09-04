c7a8029c1173d2bcf36f63510e64c55776d60c07
Danielle Cucinotta <Danielle.cucinotta@gmail.com>
2018-09-04 11:32:11 -0700

    feed css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import friendsMobile from '../../styles/assets/friendsMobile.jpg';
 import friendsDesktop from '../../styles/assets/friendsDesktop.jpg';
+import Logo from '../../styles/assets/logo.png'
 
 function Header() {
     return (
       <div className="header_parent">
+        <div className="header_title big"><img className='logo' src={Logo} alt='logo' /></div>
         <img className="header_image_desktop big" src={friendsDesktop} alt="friends"/>
+        <div className="header_title small"><img className='logo' src={Logo} alt='logo' /></div>
         <img className="header_image_mobile small" src={friendsMobile} alt="friends"/>
       </div>
     );
